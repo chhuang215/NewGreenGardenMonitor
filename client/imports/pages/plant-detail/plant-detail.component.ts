52377a10095c58dfc6d5314db51cbcb494a5326c
Chih-Hsuan Huang <gemhuang2151992@gmail.com>
2016-11-24 19:44:42 -0700

    added mobile config; revisit layout of device status; minor adjust to plant-detail

--- client/imports/pages/plant-detail/plant-detail.component.ts
+++ client/imports/pages/plant-detail/plant-detail.component.ts
@@ -1,56 +1,12 @@
 import { Component} from "@angular/core";
 import { NavParams } from 'ionic-angular';
 import style from "./plant-detail.component.scss";
-
+import template from "./plant-detail.component.html";
 import {RecipeVideoComponent} from "./recipe-video.component";
 
 @Component({
 
-  template:`
-  <ion-header>
-    <ion-navbar>
-      <ion-title>
-        Plant Detail - {{ plant.id }}. {{plant.type}}
-      </ion-title>
-    </ion-navbar>
-  </ion-header>
-  <ion-content padding>
-    <ion-grid>
-      <ion-row>
-        <ion-col width-50>
-          <img src={{plant.img}} alt={{plant.type}}>
-        </ion-col>
-        <ion-col width-50><h2>Days: 15</h2></ion-col>
-      </ion-row>
-    </ion-grid>
-    <div>{{plant.description}}</div>
-    <br>
-    <ion-list>
-      <ion-list-header>
-        Recipes
-      </ion-list-header>
-      <ion-item *ngFor="let recipe of plant.recipes">
-        <div class="recipeTitle">
-          <h2 (click)="toggleRecipe(recipe)">{{recipe.name}}  <span class="difficulty">Difficulty: {{recipe.difficulty}}</span></h2>
-        </div>
-
-        <div *ngIf="recipe.show">
-          <br>
-          <h2> Ingrediants </h2>
-          <ul>
-            <li *ngFor="let ingredient of recipe.ingredients">
-              {{ingredient.quantity}} {{ingredient.technique}} {{ingredient.name}}
-            </li>
-          </ul>
-
-          <recipe-video src={{recipe.video.src}}></recipe-video>
-
-        </div > <!-- /recipe-->
-      </ion-item>
-    </ion-list>
-  </ion-content>
-
-  `,
+  template,
   styles:[style],
 })
 
---
+++ mobile-config.js
@@ -0,0 +1,50 @@
+App.icons({
+//   // iOS
+//   'iphone': 'resources/icons/icon-60x60.png',
+//   'iphone_2x': 'resources/icons/icon-60x60@2x.png',
+//   'iphone_3x': 'resources/icons/icon-60x60@3x.png',
+//   'ipad': 'resources/icons/icon-76x76.png',
+//   'ipad_2x': 'resources/icons/icon-76x76@2x.png',
+
+//   // Android
+
+  'android_mdpi': 'resources/icons/icon48x48.png',
+  'android_hdpi': 'resources/icons/icon72x72.png',
+  'android_xhdpi': 'resources/icons/icon96x96.png',
+
+    'android_xxhdpi':'resources/icons/icon144x144.png', //(144x144)
+    'android_xxxhdpi':'resources/icons/icon192x192.png' //(192x192)
+});
+
+ App.launchScreens({
+//   // iOS
+//   'iphone': 'resources/splash/splash-320x480.png',
+//   'iphone_2x': 'resources/splash/splash-320x480@2x.png',
+//   'iphone5': 'resources/splash/splash-320x568@2x.png',
+//   'iphone6': 'resources/splash/splash-375x667@2x.png',
+//   'iphone6p_portrait': 'resources/splash/splash-414x736@3x.png',
+//   'iphone6p_landscape': 'resources/splash/splash-736x414@3x.png',
+
+//   'ipad_portrait': 'resources/splash/splash-768x1024.png',
+//   'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+//   'ipad_landscape': 'resources/splash/splash-1024x768.png',
+//   'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+
+//   // Android
+
+  //'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  'android_mdpi_portrait': 'resources/splash/splash320x480.png',
+  //'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  'android_hdpi_portrait': 'resources/splash/splash480x800.png',
+ // 'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  'android_xhdpi_portrait': 'resources/splash/splash720x1280.png',
+  //'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+//  android_mdpi_portrait (320x470)
+// android_mdpi_landscape (470x320)
+// android_hdpi_portrait (480x640)
+// android_hdpi_landscape (640x480)
+// android_xhdpi_portrait (720x960)
+// android_xhdpi_landscape (960x720)
+// android_xxhdpi_portrait (1080x1440)
+// android_xxhdpi_landscape (1440x1080)
+ });
